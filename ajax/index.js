cdea88dce920586b911fc44ba3f61ec845d57474
chengkai <17701523921@163.com>
2017-11-27 16:36:46 +0800

    手写ajax，以及promise方式实现原生ajax

--- ajax/index.js
+++ ajax/index.js
@@ -12,13 +12,14 @@ function ajax( type, url, data, callback ) {
     var xmlHttp = new XMLHttpRequest(),
         context = this,
         urls = data ? url+'?' : url;
-
+    //监听变化
     xmlHttp.onreadystatechange = function() {
+        //请求成功
         if( xmlHttp.readyState === 4 && xmlHttp.status === 200 ) {
             callback.call( context, JSON.parse( xmlHttp.responseText ) )
         }
     }
-
+    //如果是get方式请求并且需要穿参
     if( type === 'GET' && data ) {
         for( key in data ) {
             urls += ( key + '=' + data[ key ] );
@@ -42,18 +43,19 @@ ajax( 'GET', './data.json', null, function(res) {
 
 //封装ajax方法
 function ajax( type, url, data ) {
-
+    //new 一个 promise对象
     return new Promise( (resolve, reject) => {
         var xmlHttp = new XMLHttpRequest(),
             context = this,
             urls = data ? url+'?' : url;
-
+        //监听变化
         xmlHttp.onreadystatechange = function() {
+            //请求成功
             if( xmlHttp.readyState === 4 && xmlHttp.status === 200 ) {
                 resolve( JSON.parse( xmlHttp.responseText ) )
             }
         }
-
+        //如果是get方式请求并且需要穿参
         if( type === 'GET' && data ) {
             for( key in data ) {
                 urls += ( key + '=' + data[ key ] );
