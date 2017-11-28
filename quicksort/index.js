1904ce01303f4c1a2e86602b75604fd0ed77d189
chengkai <17701523921@163.com>
2017-11-28 16:10:01 +0800

    update

---
+++ quicksort/index.js
@@ -0,0 +1,23 @@
+/*
+*   @ 快速排序封装    
+*/
+
+function quickSort( arr ) {
+
+    if( arr.length <= 1 ) return arr;
+
+    var pivotIndex = Math.floor( arr.length / 2 ),
+        pivot = arr.splice( pivotIndex, 1 )[0],
+        left = [],
+        right = [],
+        i = 0;
+    for ( ; i < arr.length; i++) {
+        if( arr[i] < pivot ) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return quickSort(left).concat( [pivot], quickSort(right) );
+}
